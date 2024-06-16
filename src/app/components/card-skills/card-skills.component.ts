7f6217c168b5a11d5e886f680e63c1650891ad60
Keidson Roby <keidsondesigner@gmail.com>
2024-06-15 22:34:53 -0400

    feat: Criado Component Card Skills

---
+++ src/app/components/card-skills/card-skills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-skills.component.html',
+  styleUrl: './card-skills.component.scss'
+})
+export class CardSkillsComponent {
+
+}
