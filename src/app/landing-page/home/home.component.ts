1a55897c855f8fc9f522960696095cba515b1be4
Keidson Roby <keidsondesigner@gmail.com>
2024-06-15 22:36:21 -0400

    feat: Implementado Components na Home page

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -1,10 +1,24 @@
+import { HeroComponent } from './../../components/hero/hero.component';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SocialLinksComponent } from '../../components/social-links/social-links.component';
+import { CardProjectComponent } from '../../components/card-project/card-project.component';
+import { CardSkillsComponent } from '../../components/card-skills/card-skills.component';
+import { AboutComponent } from '../../components/about/about.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    SocialLinksComponent,
+    AboutComponent,
+    CardProjectComponent,
+    CardSkillsComponent,
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
