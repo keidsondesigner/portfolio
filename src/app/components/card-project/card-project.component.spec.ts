8de414deac92b570011dbcfa29b81f10cef16c2a
Keidson Roby <keidsondesigner@gmail.com>
2024-06-17 22:35:27 -0400

    test: Criado teste unitários do Card Component

--- src/app/components/card-project/card-project.component.spec.ts
+++ src/app/components/card-project/card-project.component.spec.ts
@@ -11,7 +11,7 @@ describe('CardProjectComponent', () => {
       imports: [CardProjectComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(CardProjectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,28 @@ describe('CardProjectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render project title and link correctly', () => {
+    const titles: HTMLHeadingElement[] = fixture.nativeElement.querySelectorAll('.container-card-project__content-cards__card h3');
+    expect(titles.length).toBe(6);
+
+    expect(titles[0].textContent?.trim()).toBe('JWT Vue 3 e Firebase');
+    const links = fixture.nativeElement.querySelectorAll('.container-card-project__content-cards__card a');
+    expect(links[0].getAttribute('href')).toBe('https://quasar-supabase-estoque.vercel.app/login');
+  });
+
+  it('should render project descriptions correctly', () => {
+    const descriptions: HTMLParagraphElement[] = fixture.nativeElement.querySelectorAll('.container-card-project__content-cards__card p');
+    expect(descriptions.length).toBe(6);
+
+    expect(descriptions[1].textContent?.trim()).toBe('Aplicativo sendo desenvolvido em Ionic e Capacitor. Acesse pelo celular');
+  });
+
+  it('should render correct icons for projects', () => {
+    const icons: HTMLElement[] = fixture.nativeElement.querySelectorAll('.container-card-project__content-cards__card i');
+    expect(icons.length).toBe(6);
+
+    expect(icons[2].classList).toContain('bi-bookmark-check');
   });
 });
