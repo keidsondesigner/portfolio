4eaa10687b9afbbe0ee924304312edcfb0b21760
Keidson Roby <keidsondesigner@gmail.com>
2024-06-17 22:34:05 -0400

    test: Criado teste unitários do Hero Component

--- src/app/components/hero/hero.component.spec.ts
+++ src/app/components/hero/hero.component.spec.ts
@@ -11,7 +11,7 @@ describe('HeroComponent', () => {
       imports: [HeroComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,33 @@ describe('HeroComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title correctly', () => {
+    const titleElement: HTMLHeadingElement = fixture.nativeElement.querySelector('.container-hero__content-item__title');
+    expect(titleElement.textContent).toContain('Olá, eu sou o Keidson Roby :)');
+  });
+
+  it('should render welcome message correctly', () => {
+    const welcomeMessageElement: HTMLParagraphElement = fixture.nativeElement.querySelector('.container-hero__content-item__paragraph-1');
+    expect(welcomeMessageElement.textContent).toContain('seja bem vindo!');
+  });
+
+  it('should render description paragraphs correctly', () => {
+    const paragraphs: HTMLParagraphElement[] = fixture.nativeElement.querySelectorAll('.container-hero__content-item__paragraph-2, .container-hero__content-item__paragraph-3');
+
+    expect(paragraphs.length).toBe(2);
+    expect(paragraphs[0].textContent).toContain('Desenvolvedor Frontend Angular & UI Designer desde 2018');
+    expect(paragraphs[1].textContent).toContain('Construindo experiências visuais e interativas para entregar o melhor projeto.');
+  });
+
+  it('should render LinkedIn button with correct href', () => {
+    const linkedinButton: HTMLAnchorElement = fixture.nativeElement.querySelector('.ontainer-hero__content-item__btn-1');
+    expect(linkedinButton.href).toBe('https://www.linkedin.com/in/keidsonroby/');
+  });
+
+  it('should render Contact button with correct href', () => {
+    const contactButton: HTMLAnchorElement = fixture.nativeElement.querySelector('.ontainer-hero__content-item__btn-2');
+    expect(contactButton.href).toBe('https://api.whatsapp.com/send?phone=5592994571858');
   });
 });
