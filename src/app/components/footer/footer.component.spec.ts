ffdf4a56b53deae72391046b34108cadc7d33450
Keidson Roby <keidsondesigner@gmail.com>
2024-06-17 22:36:02 -0400

    test: Criado teste unitários do Footer Component

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -11,7 +11,7 @@ describe('FooterComponent', () => {
       imports: [FooterComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,18 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain copyright text', () => {
+    const copyrightText: HTMLParagraphElement = fixture.nativeElement.querySelector('.container-footer__content-title__title');
+    expect(copyrightText.textContent).toContain('Copyright © 2024 Keidson Roby');
+  });
+
+  it('should contain three social links', () => {
+    const socialLinks: HTMLAnchorElement[] = fixture.nativeElement.querySelectorAll('.container-footer__content-links-social-item a');
+    expect(socialLinks.length).toBe(3);
+    expect(socialLinks[0].getAttribute('href')).toBe('https://x.com/KeidsonRoby');
+    expect(socialLinks[1].getAttribute('href')).toBe('https://github.com/keidsondesigner');
+    expect(socialLinks[2].getAttribute('href')).toBe('https://www.linkedin.com/in/keidsonroby/');
   });
 });
